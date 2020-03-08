a90ea3037d39b844dc79978d45edd9f3b43b2a1a
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-07 22:12:34 -0500

    commit

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -13,10 +13,8 @@ public class BrowserFactory {
     public static WebDriver getDriver(String browser ){
 
 
-        String os = System.getProperty("os.name").toLowerCase();
-        if(os.contains("mac")&& browser.equalsIgnoreCase("edge")) {
-            return null;
-        }  else if(browser.equalsIgnoreCase("chrome")){
+
+            if(browser.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().version("79.0").setup();
            return   new ChromeDriver();
         } else if (browser.equalsIgnoreCase("firefox")){
